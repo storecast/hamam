6dd39563d764e3a3078bcf4fa01e9e310f28395d
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-06-11 15:56:51 +0200

    SKNS-4218 | Return json in case of 40* status codes.

--- hamam/api/views.py
+++ hamam/api/views.py
@@ -1,4 +1,4 @@
-from flask import request, Blueprint, current_app, jsonify, abort
+from flask import request, Blueprint, current_app, jsonify
 from barrel_reaktor.document.models import Document
 from holon import ReaktorAuthError, ReaktorArgumentError
 from ..session import SessionStore
@@ -7,7 +7,7 @@ from ..session import SessionStore
 mod = Blueprint('api', __name__)
 
 
-@mod.route('/document/<doc_id>')
+@mod.route('/document/<doc_id>/')
 def document_view(doc_id):
     cookie_name = current_app.config['SESSION_COOKIE_NAME']
     session_id = request.cookies.get(cookie_name)
@@ -16,6 +16,6 @@ def document_view(doc_id):
     try:
         document = Document.get_by_id(token, doc_id)
         path = Document.get_doc_path(token, doc_id, document.is_user)
-    except ReaktorAuthError: abort(403)
-    except ReaktorArgumentError: abort(404)
+    except ReaktorAuthError: return jsonify(), 403
+    except ReaktorArgumentError: return jsonify(), 404
     return jsonify(extracted_epub_path=path)
