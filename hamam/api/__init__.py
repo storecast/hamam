343d41965fde8e29d579b63135303b967665c23c
Iurii Kudriavtsev <iurii.kudriavtsev@txtr.com>
2014-06-11 14:25:25 +0200

    SKNS-4218 | Adding support for document info url.

--- hamam/api/__init__.py
+++ hamam/api/__init__.py
@@ -1,12 +1,15 @@
 from flask import g, current_app
 from werkzeug.local import LocalProxy
 from holon import Reaktor
+from barrel import config as barrel
 
 
 def get_reaktor():
     """Returns the reaktor instance from the app globals."""
     if not hasattr(g, '_reaktor'):
-        g._reaktor = Reaktor(current_app.config['REAKTOR_CONFIG'])
+        g._reaktor = Reaktor(**current_app.config['REAKTOR_CONFIG'])
     return g._reaktor
 
-reaktor = LocalProxy(get_reaktor)
+reaktor = lambda: LocalProxy(get_reaktor)
+
+barrel.configure(REAKTOR=reaktor)
--- hamam/api/views.py
+++ hamam/api/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, current_app
+from flask import request, Blueprint, current_app, jsonify, abort
+from barrel_reaktor.document.models import Document
+from holon import ReaktorAuthError, ReaktorArgumentError
+from ..session import SessionStore
 
 
 mod = Blueprint('api', __name__)
@@ -6,4 +9,13 @@ mod = Blueprint('api', __name__)
 
 @mod.route('/document/<doc_id>')
 def document_view(doc_id):
-    pass
+    cookie_name = current_app.config['SESSION_COOKIE_NAME']
+    session_id = request.cookies.get(cookie_name)
+    session = SessionStore(session_id).load()
+    token = session['reaktor_token']
+    try:
+        document = Document.get_by_id(token, doc_id)
+        path = Document.get_doc_path(token, doc_id, document.is_user)
+    except ReaktorAuthError: abort(403)
+    except ReaktorArgumentError: abort(404)
+    return jsonify(extracted_epub_path=path)
